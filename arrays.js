b9b5a422d97dd7450f041299ef565d0242b515df
Valerie Hudson <vhudsonphoto@gmail.com>
2018-03-15 23:17:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,4 +24,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
  return array.slice(1)
 }
-function 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array
+}
