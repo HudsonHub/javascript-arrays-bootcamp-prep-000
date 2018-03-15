417bcc47a51838e3173921823ac0dd096b5bab8b
Valerie Hudson <vhudsonphoto@gmail.com>
2018-03-15 23:15:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,6 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
- return [array.slice(1)]
+ return array.slice(1)
  
 }
